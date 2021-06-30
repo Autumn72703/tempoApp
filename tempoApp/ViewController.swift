80041d81763743b65ac487905cdf14d2931835a2
Autumn Halfpenny <autumnhalfpenny@yahoo.com>
2021-06-30 15:02:37 -0500

    Rounded corners and added actions and outlets

--- tempoApp/ViewController.swift
+++ tempoApp/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minutesLabel: UILabel!
+    
+    @IBOutlet weak var secondsLabel: UILabel!
+    
+    @IBAction func resetLogoButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetLogoButton2(_ sender: Any) {
+    }
+    
+    @IBAction func startButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func workTime(_ sender: UIButton) {
+    }
+    
+    @IBAction func shortBreak(_ sender: UIButton) {
+    }
+    
+    @IBAction func longBreak(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //workTime.layer.cornerRadius = 25.0
     }
 
-
+    
 }
 
